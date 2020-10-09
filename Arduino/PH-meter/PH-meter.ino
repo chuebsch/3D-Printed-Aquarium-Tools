06b12b128bd1961d6de8eeb59821f367545d7edf
Christian Hübschle <chuebsch@moliso.de>
2020-10-09 16:01:20 +0200

    new menus in arduino code.

--- Arduino/PH-meter/PH-meter.ino
+++ Arduino/PH-meter/PH-meter.ino
@@ -1,4 +1,5 @@
 
+
 /*******************************************************************************
 
 
@@ -21,14 +22,16 @@ LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 20 char
 //#define DEBUG 1
 
 #define PH_SENSOR_PIN           A1
-#define AVG_ARRAY_SIZE          9
+#define AVG_ARRAY_SIZE          19
 #define DELAY_BETWEEN_SAMPLES   20 //ms
 #define VOLTAGE                 5  //V
 #define VOLTAGE_REF             1023
-#define MENUENTRYS              7
+#define MENUENTRYS              9
 #define ECA                     4
 #define ECB                     5
 #define FANPIN                  6
+#define RELAIS1                 9
+#define RELAIS2                10
 const byte sf =(byte) sizeof(float);
 
 const byte T1pin = A0;
@@ -52,6 +55,10 @@ float xph9=0.833f;
 float yph4=4.0f;
 float yph7=6.86f;
 float yph9=9.0f;
+float pHmin=15.0f;
+float pHmax=-1.0f;
+float Tmin=50.0f;
+float Tmax=-1.0f;
 
 uint32_t avgValue   = 0;
 uint32_t avgValueT1 = 0;
@@ -67,15 +74,20 @@ bool firstSamplingT1 = true;
 bool firstSamplingT2 = true;
 float sensorVoltage;
 float phValue;
+float ecValue=0.0;
 byte menu             = 0;
 byte amenu            = 0; 
-uint16_t fanspeed         = 0;
+uint16_t fanspeed     = 0;
 OneButton menuButton(12, true);
 OneButton okButton  (11, true);
+unsigned long nextDispTime = 0ul;
+unsigned long nextECTime = 0ul;
 
-bool yes=false;//OK button
-bool info=true;//verbose extra info on display
-bool measureEC=false;//enable EC measurement
+bool yes =            false;//OK button
+bool info =           true;//verbose extra info on display
+bool measureEC =      true;//enable EC measurementa
+bool lcd_backlight =  true;//what to sleep at night turn off the light? 
+bool blowFish =       true;//enable fan
 
 void fillBuffer();
 void getFloatingAvg();
@@ -233,6 +245,16 @@ void sampling(){
   }
 }
 
+float sigma(){
+  double f,DM=0.0f,DS=0.0f;
+  for (int i=0; i<AVG_ARRAY_SIZE; i++){
+    f=analogValBuf[i];
+    DM+=f;
+    DS+=f*f;
+  }
+  return sqrt((DS/AVG_ARRAY_SIZE)-((DM/AVG_ARRAY_SIZE)*(DM/AVG_ARRAY_SIZE)));
+}
+
 void samplingT1(){
   if(firstSamplingT1){
     fillBufferT1();
@@ -268,6 +290,15 @@ void doMenuClick(){
   menu%=MENUENTRYS;
 }
 
+void doMenuLongClick(){
+  lcd_backlight=!lcd_backlight;
+  if (lcd_backlight) lcd.backlight();
+  else {
+    lcd.noBacklight(); 
+    menu=0;
+  }
+}
+
 void doMenuDClick(){
   menu+=MENUENTRYS;//this way menu never gets negative
   menu--;  
@@ -278,7 +309,20 @@ void doOKClick(){
   yes=true;  
 }
 
+void doOKDClick(){
+  if (menu==0) {
+    measureEC=!measureEC; 
+    nextECTime = 0ul;
+}
+}
 
+void doOKLongClick(){
+  lcd.init();                      // initialize the lcd
+  lcd.init();
+  lcd.clear();
+  menu=0;
+  nextDispTime = 0ul;
+}
 // setup
 
 void setup() {
@@ -288,8 +332,11 @@ void setup() {
   lcd.noAutoscroll();
   lcd.clear();
   menuButton.attachClick(doMenuClick);
-  okButton.attachClick(doOKClick);
   menuButton.attachDoubleClick(doMenuDClick);
+  menuButton.attachLongPressStop(doMenuLongClick);
+  okButton.attachClick(doOKClick);
+  okButton.attachDoubleClick(doOKDClick);
+  okButton.attachLongPressStart(doOKLongClick);
 #ifdef DEBUG  
   Serial.begin(9600);
 #endif
@@ -308,16 +355,24 @@ void setup() {
   lcd.setCursor(0, 0);
   pinMode(ECA, OUTPUT);
   pinMode(ECB, OUTPUT);
+  pinMode(RELAIS1, OUTPUT);
+  pinMode(RELAIS2, OUTPUT);
   digitalWrite(ECA, LOW);
   digitalWrite(ECB, LOW);
+  digitalWrite(RELAIS1, HIGH);
+  digitalWrite(RELAIS2, HIGH);
   lcd.clear();
 }
 
 float conductivity(){
+  analogWrite(FANPIN,0);
   float rs=2200*2;
   int a6=0,a7=0;
   unsigned long diff1=0;
-  for (int i=0; i<100; i++){
+  digitalWrite(RELAIS1, LOW);
+  digitalWrite(RELAIS2, LOW);
+  delay(300); 
+  for (int i=0; i<200; i++){
     digitalWrite(ECA, HIGH);
     digitalWrite(ECB, LOW);
     delayMicroseconds(150);//50mus
@@ -334,11 +389,11 @@ float conductivity(){
     diff1+=abs(a6-a7);
   }  // 1/(550)us = 1.8kHz
 
-  float r1 = (1023.0/(1023-((float)diff1/200.0)) - 1.0) *  rs;    
+  float r1 = (1023.0/(1023-((float)diff1/400.0)) - 1.0) *  rs;    
   digitalWrite(ECA, LOW);
   digitalWrite(ECB, LOW);
-  digitalWrite(ECA, HIGH);
-  digitalWrite(ECB, HIGH);
+  digitalWrite(RELAIS1, HIGH);
+  digitalWrite(RELAIS2, HIGH);
   return (1/r1 * 1000000)*40.8;
 }
 
@@ -346,7 +401,6 @@ float conductivity(){
 
 void loop() {
   unsigned long nowis = millis();
-  static unsigned long nextDispTime = 0ul;
 
   menuButton.tick();
   okButton.tick();
@@ -355,6 +409,9 @@ void loop() {
     sampling();
     sensorVoltage = (float)avgValue * VOLTAGE / VOLTAGE_REF;
     phValue = steigung * sensorVoltage + achsenabschnitt;
+    float sig=sigma();
+    sig*= abs(steigung * VOLTAGE / VOLTAGE_REF);
+    int8_t dig = min(-1*floor(log(3*sig)/log(10)),1);
 #ifdef DEBUG  
     Serial.print(F("pH value: "));
     Serial.println(phValue);
@@ -369,10 +426,12 @@ void loop() {
     float R2=(1023.0/(1023-((float)avgValueT2)) - 1.0) * R2a;
     lcd.setCursor(0, 0);
     lcd.print(F("pH: "));
-    lcd.print(phValue);
-    lcd.print(F("  "));
-    if (info) 
-    {
+    lcd.print(phValue,dig);
+    lcd.print(F(" "));
+    lcd.printByte(229);//sigma
+    lcd.print(sig,dig);
+    lcd.print(F(" "));
+    if (info) {
       lcd.print(avgValue);
       lcd.print(F(" "));
       lcd.setCursor(0, 1);
@@ -395,21 +454,51 @@ void loop() {
     lcd.printByte(1);//grad
     lcd.print("C");  
     lcd.print("   ");
-    fanspeed = max((int)((((t1+t2*5)/6.0)-21)*44.0), 0);
+    if (!measureEC){
+      pHmin=min(pHmin,phValue);
+      pHmax=max(pHmax,phValue);
+      Tmin=min((t1+t2*5)/6.0,Tmin);
+      Tmax=max((t1+t2*5)/6.0,Tmax);
+      if (!info){
+        lcd.setCursor(0, 1);
+        lcd.print(F("pH:"));
+        lcd.print(pHmin,1);
+        lcd.print(F(" "));
+        lcd.print(pHmax,1);
+        lcd.print(F(" "));
+        lcd.setCursor(0, 2);
+        lcd.printByte(4);//temp
+        lcd.print(Tmin,1); 
+        lcd.printByte(1);//grad
+        lcd.print(F("C "));  
+        lcd.print(Tmax,1); 
+        lcd.printByte(1);//grad
+        lcd.print(F("C "));  
+      }
+    }
+    uint16_t oldfan=fanspeed;
+    fanspeed = (uint16_t) max((int)((((t1+t2*5)/6.0)-21.5)*44.0), 0);
     fanspeed = min(255, fanspeed);
-    fanspeed = (fanspeed < 20)?0: fanspeed;
-    analogWrite(FANPIN,fanspeed);
+    if (oldfan==0){//hysteresis
+      fanspeed = (fanspeed < 45)?0: fanspeed;//22.5grad
+    }else{
+      fanspeed = (fanspeed < 23)?0: fanspeed;//22grad
+    }
+    analogWrite(FANPIN,(blowFish)?fanspeed:0);
     if (info){
       lcd.print(fanspeed);   
-      lcd.print(" ");
+      lcd.print((blowFish)?" ":"! ");
     }
     if (measureEC){
       lcd.setCursor(0, 2);  
-      float ec=conductivity();
-      if (ec>0){
-        lcd.print(ec);
-        lcd.printByte(228);
-        lcd.print("S");  
+      if ((nowis - nextECTime) > 60000ul){//5000ul){//
+      ecValue = conductivity();
+      nextECTime=nowis;
+      }
+      if (ecValue>0){
+        lcd.print(ecValue);
+        lcd.printByte(228);//micro
+        lcd.print(F("S   "));  
         lcd.print("   ");
       } else if (info){
         lcd.print(avgValueT1);
@@ -430,27 +519,27 @@ void loop() {
       lcd.print((menu==2)?">":" ");
       lcd.print(F("Calibrate pH7 "));
       lcd.setCursor(0, 2); 
-
       lcd.print((menu==3)?">":" ");
       lcd.print(F("Calibrate pH9 "));
       lcd.setCursor(0, 3); 
       lcd.print((menu==4)?">":" ");
       lcd.print(F("Fan full speed "));
     }else{
-      lcd.setCursor(0, 0);  
-      lcd.print((menu==3)?">":" ");
-      lcd.print(F("Calibrate pH9 "));
-      lcd.setCursor(0, 1); 
-      lcd.print((menu==4)?">":" ");
-      lcd.print(F("Fan full speed "));
-      lcd.setCursor(0, 2); 
+      lcd.setCursor(0, 0); 
       lcd.print((menu==5)?">":" ");
       lcd.print(F("Debug info: "));
       lcd.print((info)?"[yes]":"[no] ");
-      lcd.setCursor(0, 3); 
+      lcd.setCursor(0, 1); 
       lcd.print((menu==6)?">":" ");
       lcd.print(F("Enable EC: "));
       lcd.print((measureEC)?"[yes]":"[no] ");
+      lcd.setCursor(0, 2); 
+      lcd.print((menu==7)?">":" ");
+      lcd.print(F("Reset min/max: "));
+      lcd.setCursor(0, 3); 
+      lcd.print((menu==8)?">":" ");
+      lcd.print(F("Enable FAN: "));
+      lcd.print((blowFish)?"[yes]":"[no] ");
     }//*/
     if(yes){
       switch (menu){
@@ -460,6 +549,17 @@ void loop() {
         case 4: fanspeed=255;break;
         case 5: info=!info; break;
         case 6: measureEC=!measureEC;break;
+        case 7: 
+                pHmin=15.0f;
+                pHmax=-1.0f;
+                Tmin=50.0f;
+                Tmax=-1.0f;
+                break;
+        case 8:
+                blowFish=!blowFish;
+                fanspeed=(blowFish)?fanspeed:0;
+                analogWrite(FANPIN,fanspeed);
+                break;
         default: 
         lcd.clear();
         lcd.print(F("Not implementet yet!"));
@@ -468,7 +568,7 @@ void loop() {
       }
       fanspeed=max(fanspeed, 0);
       fanspeed%=256;
-      if (fanspeed) fanspeed= (fanspeed<15)?15:fanspeed;
+      if (fanspeed) fanspeed= (fanspeed<35)?35:fanspeed;
       //analogWrite(6,fanspeed);
       yes=false;
     }
